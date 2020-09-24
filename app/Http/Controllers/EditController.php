44439489a6a570359621d584ccb0d00a6b6ec035
shawyer <696656x@student.swin.edu.au>
2020-09-24 13:19:18 +1000

    remove brackets

--- app/Http/Controllers/EditController.php
+++ app/Http/Controllers/EditController.php
@@ -10,7 +10,7 @@ class EditController extends Controller
 {
     public function EditItem(Request $request) {
         $Item = $request->input('Item_Names');
-        $ItemId = Item::where(("Item_Name"), $Item)->first(); // Get the item name's record
+        $ItemId = Item::where("Item_Name", $Item)->first(); // Get the item name's record
         $ItemId->Item_Name = $request->input("Item_Name_Change");
         $ItemId->save(); //push to table
         return view("confirmation"); // head to confirmation page.
@@ -18,7 +18,7 @@ class EditController extends Controller
     public function EditItemQuantity(Request $request) {
 
         $Item = $request->input('Item_Name_Quantity');
-        $ItemId = Item::where(("Item_Name"), $Item)->first(); // Get the item name's record
+        $ItemId = Item::where("Item_Name", $Item)->first(); // Get the item name's record
         if($request->input('operation') == "remove") {
             $ItemId->Item_Remaining  -= (int)$request->input("Item_Quantity");
         }
@@ -31,7 +31,7 @@ class EditController extends Controller
     public function EditItemSaleAmount(Request $request) {
 
         $ItemSale = (int)$request->input('SaleID');
-        $ItemSaleNew = ItemSale::where(("id"), $ItemSale)->first(); // Get the item name's record
+        $ItemSaleNew = ItemSale::where("id", $ItemSale)->first(); // Get the item name's record
         $ItemSaleNew->Item_Sold = (int)$request->input('Item_Quantity_Change');
         $ItemSaleNew->save();
         return view("confirmation");
