cb01284f33326084eb5259f4e9ff880739b2e712
Giovanni <gianni35011@gmail.com>
2020-10-14 23:25:20 +1100

    Added CVS package

---
+++ app/Http/Controllers/GenerateCVS.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+namespace 
+
+use Illuminate\Http\Request;
+
+class GenerateCVS extends Controller
+{
+    public function GenerateCVSTest(){
+        
+    }
+}
