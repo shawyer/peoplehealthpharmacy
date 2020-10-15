25d952bdd0ac326c03600f0543cedeb8cac8b822
shawyer <696656x@student.swin.edu.au>
2020-10-15 22:21:37 +1100

    import frontend contend and scaffold add.blade

---
+++ resources/views/layouts/components/header.blade.php
@@ -0,0 +1,17 @@
+<head>
+    @include('layouts.components.meta')
+    @yield('meta')
+    <title>@yield('title')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
+    @yield('header')
+</head>
---
+++ resources/views/layouts/components/verticalnavadd.blade.php
@@ -0,0 +1,22 @@
+<div class = "vertical_nav">
+    <img src="{{ asset('Assets/background/Combined Shape.png') }}" width="250" height="1080" alt="" id = "overlay"/>
+    <img src="{{ asset('Assets/background/Logo.png') }}" width="181" height="95" alt="" id = "logo"/>
+    <img src="{{ asset('Assets/active/Active Color.png') }}" width="202" height="219" alt="" id="activ_col"/>
+    
+    <a href="reports.html">
+        <img src="{{ asset('Assets/passive/Reports.png') }}" width="90" height="74" alt="" id="reports" />
+    </a>
+    
+    
+    <a href="predictions.html">
+        <img src="{{ asset('Assets/passive/Predictions.png') }}" width="132" height="95" alt="" id="predictions" />
+    </a>
+
+    
+    <img src="{{ asset('Assets/active/New sale.png') }}" width="118" height="88" alt="" id="new_sale" />
+    
+    
+    <a href="modifysales.html">
+        <img src="{{ asset('Assets/passive/Modify Sale.png') }}" alt="" width="118" height="80" id="modify_sale"/>
+    </a>
+</div>
