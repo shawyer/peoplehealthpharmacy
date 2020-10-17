cb25ec4875e038f6ebaef28d330c7c17da53c948
LuTeck97 <100088365@student.swin.edu.au>
2020-10-17 21:23:52 +1100

    sort by item Name,date,qty done

--- app/Http/Controllers/DisplayController.php
+++ app/Http/Controllers/DisplayController.php
@@ -25,15 +25,133 @@ class DisplayController extends Controller
         return view ('displayItem', compact('item'));
     }
 
-    public function displayMonthlyItems(){
+  public function displayMonthlyItems(/*$sort*/){
         $monthSales = DB::table('sales')
         ->join('itemsale','sales.id', '=', 'itemsale.Sales_Id')
         ->join('items','itemsale.Items_Id', '=', 'items.id')
         ->select('items.Item_Name', 'items.Item_Remaining', 'itemsale.Item_Sold', 'sales.created_at')
+        //->order('itemsale.Item_Sold')
         ->whereMonth('sales.created_at', Carbon::now()->month)->get();
         #$monthSales = \App\Models\Sale::all();
         #dd($monthSales);
+        $sort='name';//'date';'qty';
+
+        if($sort=='name')
+        {
+          $monthSales=$this->sortbyName('d',$monthSales);
+        }
+        else if($sort='date'){
+          $monthSales=$this->sortbyDate('d',$monthSales);
+        }
+        elseif ($sort='qty') {
+          $monthSales=$this->sortbyQty('d',$monthSales);
+        }
+
         return view ('monthlysales', compact('monthSales'));
     }
 
+
+    public function sortbyName($order,$array)
+    {
+      $size=count($array);
+      if($order=='a')
+      {
+        for($i=0;$i<$size;$i++)
+        {
+          for($j=0;$j<$size-1;$j++)
+          {
+            if($array[$j]->Item_Name>=$array[$j+1]->Item_Name)
+            {
+              $temp=$array[$j];
+              $array[$j]=$array[$j+1];
+              $array[$j+1]=$temp;
+            }
+          }
+        }
+      }else if($order=='d')
+      {
+        for($i=0;$i<$size;$i++)
+        {
+          for($j=0;$j<$size-1;$j++)
+          {
+            if($array[$j]->Item_Name<=$array[$j+1]->Item_Name)
+            {
+              $temp=$array[$j];
+              $array[$j]=$array[$j+1];
+              $array[$j+1]=$temp;
+            }
+          }
+        }
+      }
+      return $array;
+    }
+
+    public function sortbyDate($order,$array)
+    {
+      $size=count($array);
+      if($order=='a')
+      {
+        for($i=0;$i<$size;$i++)
+        {
+          for($j=0;$j<$size-1;$j++)
+          {
+            if($array[$j]->created_at>=$array[$j+1]->created_at)
+            {
+              $temp=$array[$j];
+              $array[$j]=$array[$j+1];
+              $array[$j+1]=$temp;
+            }
+          }
+        }
+      }else if($order=='d')
+      {
+        for($i=0;$i<$size;$i++)
+        {
+          for($j=0;$j<$size-1;$j++)
+          {
+            if($array[$j]->created_at<=$array[$j+1]->created_at )
+            {
+              $temp=$array[$j];
+              $array[$j]=$array[$j+1];
+              $array[$j+1]=$temp;
+            }
+          }
+        }
+      }
+      return $array;
+    }
+
+    public function sortbyQty($order,$array){
+      $size=count($array);
+      if($order=='a')
+      {
+        for($i=0;$i<$size;$i++)
+        {
+          for($j=0;$j<$size-1;$j++)
+          {
+            if($array[$j]->Item_Sold>=$array[$j+1]->Item_Sold)
+            {
+              $temp=$array[$j];
+              $array[$j]=$array[$j+1];
+              $array[$j+1]=$temp;
+            }
+          }
+        }
+      }else if($order=='d')
+      {
+        for($i=0;$i<$size;$i++)
+        {
+          for($j=0;$j<$size-1;$j++)
+          {
+            if($array[$j]->Item_Sold<=$array[$j+1]->Item_Sold)
+            {
+              $temp=$array[$j];
+              $array[$j]=$array[$j+1];
+              $array[$j+1]=$temp;
+            }
+          }
+        }
+      }
+      return $array;
+    }
 }
