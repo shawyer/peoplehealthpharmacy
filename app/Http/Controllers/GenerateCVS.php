4959e74e81934f5a76c50120536f8efde7196012
Giovanni <gianni35011@gmail.com>
2020-10-19 17:58:58 +1100

    CSV export fixes

--- app/Http/Controllers/GenerateCVS.php
+++ app/Http/Controllers/GenerateCVS.php
@@ -10,14 +10,25 @@ use Carbon\Carbon;
 class GenerateCVS extends Controller
 {
     public function GenerateCVS(){
+/*        $monthSales = DB::table('sales')
+        ->join('itemsale','sales.id', '=', 'itemsale.Sales_Id')
+        ->join('items','itemsale.Items_Id', '=', 'items.id')
+        ->select('items.Item_Name', 'items.Item_Remaining', 'itemsale.Item_Sold', 'sales.created_at', 
+        'items.id as items_id', 'itemsale.Items_Id as itemsale_item_id', 'sales.created_at as sold')*/
+
         $monthSales = DB::table('sales')
         ->join('itemsale','sales.id', '=', 'itemsale.Sales_Id')
         ->join('items','itemsale.Items_Id', '=', 'items.id')
-        ->select('items.Item_Name', 'items.Item_Remaining', 'itemsale.Item_Sold', 'sales.created_at', 'items.id as items_id', 'itemsale.Items_Id as itemsale_item_id')
-        ->whereMonth('sales.created_at', Carbon::now()->month)->get();
-        
+        ->select('items.Item_Name')
+        ->whereMonth('sales.created_at', Carbon::now()->month)
+        ->get()
+        ->groupBy(function($date){
+            return Carbon::parse($date->created_at)->format('d');
+        });
+
         $csvExporter = new \Laracsv\Export();
-        $csvExporter->build($monthSales, ['Item_Name', 'Item_Remaining', 'Item_Sold', 'created_at', 'items_id', 'itemsale_item_id']);
+        $csvExporter->build($monthSales, ['Item_Name']);
+        dd($csvExporter);
         $csvExporter->download('month_sales.csv');
     }
 }
