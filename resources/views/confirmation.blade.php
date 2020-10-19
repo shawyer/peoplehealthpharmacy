0bc4919b476e85b5ae6f0385f9f1f6d6152824c5
rohan.kalanje <rohan.kalanje@gmail.com>
2020-10-19 18:41:20 +1100

    changes to submit confirmed for most post view

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -1,6 +1,23 @@
-
+<!-- 
 <div class="container">
   <p> Item has been successfully added </p>
   <a href="{{route('add')}}">Add Another</a>
 </div>
+-->
+@extends('layouts.app')
+@section('title')
+AddSalesConfirmation
+@endsection
+@section('css')
+   <link href="{{asset('css/submitconfirmed.css')}}" rel="stylesheet" type="text/css">
+@endsection
+@section('verticalnav')
+@include('layouts.components.verticalnavbase')
+@endsection
+@section('content')
 
+<img src="../Assets/Submit Confirmed Assets/Title.png" alt="" id="PageTitle" />
+		<img src="../Assets/Submit Confirmed Assets/ConfirmedText.png" id="ConfirmedText" />
+		<img src="../Assets/Submit Confirmed Assets/ConfirmBg.png" id="ConfirmBg" />
+    
+    <input type="button" value="Back" onclick="history.back()" width="320" height="110" class = "large_buttons" id="BackButton" />
---
+++ resources/views/layouts/components/verticalnavbase.blade.php
@@ -0,0 +1,24 @@
+<div class = "vertical_nav">
+    <img src="../Assets/Navigation/Background/Combined Shape.png" width="250" height="1080" alt="" id = "overlay"/>
+    <img src="../Assets/Navigation/Background/Logo.png" width="181" height="95" alt="" id = "logo"/>
+    
+    <a href="/displaymonth">
+        <img src="{{asset('Assets/Navigation/Passive/Reports.png')}}" width="90" height="74" alt="" id="reports" />
+    </a>
+    
+    <a href="/monthlyPrediciton">
+            <img src="../Assets/Navigation/Passive/Predictions.png" width="132" height="95" alt="" id="predictions" />
+    </a>
+
+    <a href="/sales">
+        <img src="../Assets/Navigation/Passive/New sale.png" width="118" height="88" alt="" id="new_sale" />
+    </a>
+    
+    <a href="/EditItem">
+        <img src="../Assets/Navigation/Passive/Modify Sale.png" alt="" width="118" height="80" id="modify_sale"/>
+    </a>
+    <a href="/add">
+        <img src="../Assets//Navigation/Passive/Manage Stock.png" alt="" width="118" height="80" id="add_stock"/>
+    </a>
+    
+</div>
