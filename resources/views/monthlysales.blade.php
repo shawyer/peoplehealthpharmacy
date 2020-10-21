b06346aaed7023c1ebd053ec38c34a7d2db44222
rohan.kalanje <rohan.kalanje@gmail.com>
2020-10-21 15:07:43 +1100

    routing button enabled for weekly sales page

--- resources/views/monthlysales.blade.php
+++ resources/views/monthlysales.blade.php
@@ -42,8 +42,8 @@ Monthly Sales
         <!-- buttons -->
         <img src="../Assets/Reports Assets/Filter.png" id="viewText" />
 
-        <input type="button" value="Weekly" onclick="history.back()" width="320" height="110" class = "large_buttons" id="weekly"/>
-        <input type="button" value="Monthly" onclick="history.back()" width="320" height="110" class = "large_buttons" id="monthly" />
+        <input type="button" value="Weekly" onclick="window.location.href='/monthlysales'" width="320" height="110" class = "large_buttons" id="weekly"/>
+        <input type="button" value="Monthly" onclick="window.location.href='/weeklysales'" width="320" height="110" class = "large_buttons" id="monthly" />
         <!-- routing for csv file download -->
         <a href="{{route('csv')}}">
             <img src="../Assets/Reports Assets/Light 🌕- Button-2. Outlined-A. Text-Enabled.png" alt="Download button" id="DownloadButton">
